aa97a7172b454a821887c7ff82c37da3e8157346
nomissbowling <nomissbowling@yahoo.co.jp>
2023-11-02 19:11:41 +0900

    version 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
-#![doc(html_root_url = "https://docs.rs/jput/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/jput/0.1.1")]
 //! puts and putc on unicode-width align for Rust
 //!
 
 use std::error::Error;
 use unicode_width::{UnicodeWidthStr, UnicodeWidthChar};
 
+use colored::{Colorize, Color, ColoredString};
+
+/// jput padding
 pub fn jput_pad(p: bool, n: i32, w: i32) {
   let l = if n < 0 { if p {0} else {-n-w} } else { if p {n-w} else {0} };
   for _ in 0..l { print!(" "); }
 }
 
+/// jputc char
 pub fn jputc(c: char, n: i32) -> Result<(), Box<dyn Error>> {
   let w = c.width().ok_or("width")? as i32; // width_cjk()
   jput_pad(true, n, w);
@@ -18,7 +22,22 @@ pub fn jputc(c: char, n: i32) -> Result<(), Box<dyn Error>> {
   Ok(())
 }
 
+/// jputc char colored
+pub fn jputc_colored(c: char, n: i32, k: Color) -> Result<(), Box<dyn Error>> {
+  jputs_colored(&format!("{}", c).color(k), n)
+}
+
+/// jputs String
 pub fn jputs(s: &String, n: i32) -> Result<(), Box<dyn Error>> {
+  let w = s.width() as i32; // width_cjk()
+  jput_pad(true, n, w);
+  print!("{}", s);
+  jput_pad(false, n, w);
+  Ok(())
+}
+
+/// jputs ColoredString
+pub fn jputs_colored(s: &ColoredString, n: i32) -> Result<(), Box<dyn Error>> {
   let w = s.width() as i32; // width_cjk()
   jput_pad(true, n, w);
   print!("{}", s);
